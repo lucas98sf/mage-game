2890d6204102346e68663e7c5ed37e2f9cacbf5c
lucas98sf <lucas98sf@gmail.com>
2024-07-01 20:59:26 -0300

    double jump + walk anim

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,10 +1,13 @@
 extends CharacterBody2D
 
-@export var SPEED = 300.0
-@export var JUMP_VELOCITY = -400.0
+var SPEED = 130.0
+var JUMP_VELOCITY = -200.0
+
+@onready var animated_sprite = $AnimatedSprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var has_double_jump = true
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -12,8 +15,12 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and (is_on_floor() or has_double_jump):
 		velocity.y = JUMP_VELOCITY
+		if not is_on_floor():
+			has_double_jump = false
+		else:
+			has_double_jump = true
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -22,5 +29,15 @@ func _physics_process(delta):
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	if direction < 0:
+		animated_sprite.flip_h = true
+	elif direction > 0:
+		animated_sprite.flip_h = false
+
+	if velocity.x != 0:
+		animated_sprite.play("run")
+	else:
+		animated_sprite.play("idle")
 
 	move_and_slide()
